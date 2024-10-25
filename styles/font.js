203f0d4f9b3625fd8f0a755687174917085cf2a0
debertjamie <debertjamie@outlook.com>
2024-10-25 23:32:33 +0700

    style: Switch to JavaScript, Add PixelGamer font

---
+++ styles/font.js
@@ -0,0 +1,5 @@
+import {Inter, Hammersmith_One} from "next/font/google";
+import localFont from 'next/font/local'
+
+export const pixelgamer = localFont({src: "./PixelGamer.otf"});
+export const hammersmith = Hammersmith_One({weight: "400", subsets: ["latin"]});

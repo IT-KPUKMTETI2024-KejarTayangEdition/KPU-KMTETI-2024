2f02600be7df7e88c761c204fbd6d0dd5018cfaf
debertjamie <debertjamie@outlook.com>
2024-10-26 17:54:01 +0700

    feat: Add coming soon for unfinished page

---
+++ common/components/coming-soon.js
@@ -0,0 +1,9 @@
+export function ComingSoon() {
+  return (
+    <div className="mx-auto w-fit my-16 text-xl">
+      <h1 className="text-2xl font-semibold">COMING SOON</h1>
+      <p>This page is still in development. Check back soon for more updates!</p>
+      <p className="text-right">- IT KejarTayang 2024</p>
+    </div>
+  )
+}

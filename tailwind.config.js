fe6d744bc66a53a5cc476da62ee34f8d50fd78f4
debertjamie <debertjamie@outlook.com>
2024-10-26 17:44:53 +0700

    style: Add extra colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,20 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./modules/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./common/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "primary-light": "rgb(253 214 166)",
+        "primary-dark": "rgb(245 180 107)",
+        "secondary-darkred": "rgb(37 18 5)",
+        "secondary-purple": "rgb(53 34 70)",
+        "background-dark": "rgb(25 0 2)",
+        "text-light": "rgb(252 252 252)",
+        "text-dark": "rgb(29 29 33)"
       },
     },
   },

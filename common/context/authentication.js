e27dbaf5bc5a47286b291cf33390303ff71e8459
debertjamie <debertjamie@outlook.com>
2024-10-30 02:35:43 +0700

    feat: Add dialog for login, persist user's session

--- common/context/authentication.js
+++ common/context/authentication.js
@@ -1,17 +1,18 @@
 "use client";
 
-import {useContext, createContext, useState, useEffect} from "react";
-import {
-  signInWithPopup,
-  signOut,
-  onAuthStateChanged,
-  GoogleAuthProvider,
-  setPersistence,
-  browserSessionPersistence
-} from "firebase/auth";
+import {useContext, createContext, useState} from "react";
+import {signInWithPopup, signOut, onAuthStateChanged, GoogleAuthProvider} from "firebase/auth";
 import {auth, db} from "@/common/libs/firebase";
 import {doc, getDoc} from "firebase/firestore";
-import {setAuthDialogue} from "@/common/components/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+  DialogFooter,
+  DialogCancel,
+} from "@/common/components/dialog";
 
 export const AuthContext = createContext({});
 
@@ -21,50 +22,61 @@ export function AuthContextProvider({children}) {
    *  @type {Function} setUser - The function to set the user object.
    */
   const [user, setUser] = useState(null);
+  const [dialog, setDialog] = useState({type: "", message: "", isVisible: false});
+
   function googleLogin() {
     if (!auth.currentUser) {
       const provider = new GoogleAuthProvider();
-      setPersistence(auth, browserSessionPersistence).then(() => {
-        const provider = new GoogleAuthProvider();
-        signInWithPopup(auth, provider).then((cred) => {
-          setUser(cred.user);
-          if (!cred.user.email.includes("@mail.ugm.ac.id")) {
-            return setAuthDialogue({
-              type: "warn",
-              message: "Welcome to KPU KMTETI 2024! Sadly, we only accept votes from students of the Department of Electrical and Information Engineering in UGM. Please sign in with your UGM email address if you'd like to vote."
-            });
-          } else {
-            useEffect(() => {
-              const unsubscribe = onAuthStateChanged(auth, (user) => {
-                const data = doc(db, "data-mahasoswa-dteti", user.displayName);
-                getDoc(data).then((snapshot) => {
-                  if (!snapshot.exists()) {
-                    return setAuthDialogue({
-                      type: "warn",
-                      message: "Welcome to KPU KMTETI 2024! Your account is not registered as a valid voter thus you do not have the permission to vote. Please contact us if you think we made a mistake!"
-                    });
-                  }
-                  return setAuthDialogue({
-                    type: "success",
-                    message: `Sign in complete. Welcome, ${cred.user.displayName}!`
-                  });
-                })
+      signInWithPopup(auth, provider).then((cred) => {
+        if (!cred.user.email.includes("@mail.ugm.ac.id")) {
+          logout();
+          return setDialog({
+            type: "rejected",
+            message: "Welcome to KPU KMTETI 2024! Unfortunately, we only accept votes from students of the Department of Electrical and Information Engineering of Gadjah Mada University. Please sign in with your UGM email address if you'd like to vote.",
+            isVisible: true,
+          });
+        } else {
+          const unsubscribe = onAuthStateChanged(auth, (user) => {
+            const data = doc(db, "data-mahasiswa-dteti", user.displayName);
+            getDoc(data).then((snapshot) => {
+              if (!snapshot.exists()) {
+                logout();
+                return setDialog({
+                  type: "rejected",
+                  message: "Welcome to KPU KMTETI 2024! Your account is not registered as a valid student of the Department of Electrical and Information Engineering on batch 2021 - 2024, thus you do not have the permission to vote. Please contact us if you think we made a mistake",
+                  isVisible: true,
+                });
+              }
+              setUser(cred.user);
+              return setDialog({
+                type: "success",
+                message: `Successfully signed in. Welcome, ${cred.user.displayName}!`,
+                isVisible: true,
               });
-              return () => unsubscribe();
-            }, [])
-          }
-        }).catch((err) => {
-          console.log(err);
-          setAuthDialogue({type: "error", message: "An error occured while signing in. Please try again."});
+            })
+          });
+          return () => unsubscribe();
+        }
+      }).catch(() => {
+        return setDialog({
+          type: "error",
+          message: "An error occurred while signing in. Please try again.",
+          isVisible: true
         });
       });
     }
+    setUser(auth.currentUser);
+    setDialog({type: "success", message: `Welcome back, ${auth.currentUser.displayName}!`, isVisible: true});
   }
 
-  function logout() {
+  function logout(showDialog = false) {
     signOut(auth);
     setUser(null);
-    setAuthDialogue({type: "success", message: "You have been logged out."});
+    if (showDialog) setDialog({type: "success", message: "You have been logged out.", isVisible: true});
+  }
+
+  function closeDialog() {
+    setDialog({type: "", message: "", isVisible: false});
   }
 
   // TODO: vote function and realtime count function
@@ -72,6 +84,24 @@ export function AuthContextProvider({children}) {
   return (
     <AuthContext.Provider value={{googleLogin, logout, user}}>
       {children}
+      <Dialog open={dialog.isVisible}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>
+              <span className="text-2xl">
+                {dialog.type === "error" ? "Error" : dialog.type === "rejected" ? "Permission Denied" : "Success!"}
+              </span>
+            </DialogTitle>
+            <DialogDescription>
+              <span className="text-lg">{dialog.message}</span>
+            </DialogDescription>
+          </DialogHeader>
+          <DialogFooter>
+            <DialogCancel onClick={closeDialog}
+                          className="px-2 py-1 rounded-lg border border-text-light">Close</DialogCancel>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </AuthContext.Provider>
   )
 }

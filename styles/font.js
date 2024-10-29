d6d8f602a8bb51387a09c13ffc072164aeb28e47
debertjamie <debertjamie@outlook.com>
2024-10-29 21:53:48 +0700

    style: Add fonts

--- styles/font.js
+++ styles/font.js
@@ -2,4 +2,6 @@ import {Inter, Hammersmith_One} from "next/font/google";
 import localFont from 'next/font/local'
 
 export const pixelgamer = localFont({src: "./PixelGamer.otf"});
+export const dalekpinpoint = localFont({src: "./DalekPinpoint.ttf"});
+export const eightbitwonder = localFont({src: "./8-bit wonder.ttf"});
 export const hammersmith = Hammersmith_One({weight: "400", subsets: ["latin"]});

b06e73d68473b6a53753d49f5161cc020cd1eb2a
hendrakrnm <hendrakurnia421@gmail.com>
2024-11-15 05:24:49 +0700

    tambah text untuk tata cara

--- common/constant/text.js
+++ common/constant/text.js
@@ -37,6 +37,14 @@ export const text = {
             title1 : `ORGANIZING COMMITTEE`,
             title2 : `KPU KMTETI 2024`,
             description : ''
-        }
+        },
+    },
+
+    tataCara : {
+        section1 : {   
+            title1 : `TATA CARA PEMILU`,
+            title2 : `KETUA KMTETI 2024`,
+            description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+        },
     },
 }

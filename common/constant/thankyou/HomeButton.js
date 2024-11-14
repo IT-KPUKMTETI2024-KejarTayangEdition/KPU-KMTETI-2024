d6046c38eed4237c3e2b4f93d7e66ec4728f5448
aevryiam <ilhamyusufwiam@mail.ugm.ac.id>
2024-11-15 02:28:06 +0700

    add thankyou page (still react)

---
+++ common/constant/thankyou/HomeButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HomeButton() {
+  return (
+    <button className="px-6 pt-5 pb-9 mt-10 w-80 max-w-full text-3xl text-center bg-orange-300 rounded-2xl text-stone-950 max-md:pl-5">
+      Kembali Ke Beranda
+    </button>
+  );
+}
+
+export default HomeButton;
---
+++ common/constant/thankyou/Technocracy.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Technocracy() {
+  return (
+    <img 
+      loading="lazy" 
+      src="https://cdn.builder.io/api/v1/image/assets/TEMP/f781fd2df671cccfb0e94b85b221e0aa10aeaa35057c0eef0e4b3fe6a8542985?placeholderIfAbsent=true&apiKey=22e29311379644cabaabda6a2d09ec99" 
+      alt="KPU KMTETI 2024 Thank You Image" 
+      className="object-contain mt-5 max-w-full aspect-[2.8] shadow-[7px_10px_14px_rgba(0,0,0,0.97)] w-[1105px]" 
+    />
+  );
+}
+
+export default Technocracy;

dd2c1bcbe51aad7fd0c7b39615a62ff8bf40ed6b
debertjamie <debertjamie@outlook.com>
2024-11-16 19:23:57 +0700

    feat: Format code, add vision and mission or candidates

---
+++ common/constant/classes.js
@@ -0,0 +1,8 @@
+export const preventImageDrag = {
+  "userDrag": "none",
+  "WebkitUserDrag": "none",
+  "MozUserSelect": "none",
+  "WebkitUserSelect": "none",
+  "MsUserSelect": "none",
+  "userSelect": "none"
+};
--- common/constant/text.js
+++ common/constant/text.js
@@ -1,50 +1,64 @@
 export const text = {
-    landingPage : {
-        section3 : {
-            descText : `TECHNOCRACY “Navigating TETI’s Road to A New Leader” merupakan akronim dari technology dan democracy. TECHNOCRACY menggabungkan inovasi teknologi dan semangat demokrasi yang diwujudkan dalam Pemilihan Ketua KMTETI yang dijalankan secara adil, transparan, dan penuh integritas. Melalui TECHNOCRACY, diharapkan dapat terpilih Ketua KMTETI 2025 yang mampu berdiri di garis depan perubahan dan membawa KMTETI menuju masa depan yang lebih baik dengan semangat keadilan. `
-        }
+  landingPage: {
+    section3: {
+      descText: `TECHNOCRACY “Navigating TETI’s Road to A New Leader” merupakan akronim dari technology dan democracy. TECHNOCRACY menggabungkan inovasi teknologi dan semangat demokrasi yang diwujudkan dalam Pemilihan Ketua KMTETI yang dijalankan secara adil, transparan, dan penuh integritas. Melalui TECHNOCRACY, diharapkan dapat terpilih Ketua KMTETI 2025 yang mampu berdiri di garis depan perubahan dan membawa KMTETI menuju masa depan yang lebih baik dengan semangat keadilan. `
+    }
+  },
+  aboutPage: {
+    section1: {
+      title1: `PEMILIHAN UMUM`,
+      title2: `KMTETI 2024`,
+      description: `Pemilihan Umum Keluarga Mahasiswa Teknik Elektro dan Teknologi Informasi Fakultas Teknik Universitas Gadjah Mada adalah sarana pelaksanaan kedaulatan mahasiswa berdasarkan Anggaran Dasar dan Anggaran Rumah Tangga Keluarga Mahasiswa Teknik Elektro dan Teknologi Informasi Fakultas Teknik Universitas Gadjah Mada.`,
     },
-    aboutPage : {
-        section1 : {
-            title1 : `PEMILIHAN UMUM`,
-            title2 : `KMTETI 2024`,
-            description : `Pemilihan Umum Keluarga Mahasiswa Teknik Elektro dan Teknologi Informasi Fakultas Teknik Universitas Gadjah Mada adalah sarana pelaksanaan kedaulatan mahasiswa berdasarkan Anggaran Dasar dan Anggaran Rumah Tangga Keluarga Mahasiswa Teknik Elektro dan Teknologi Informasi Fakultas Teknik Universitas Gadjah Mada.`,
-        },
-        section2 : {
-           title1 : `KOMISI PEMILIHAN UMUM`,
-           title2 : `KMTETI 2024`,
-           description : `Komisi Pemilihan Umum Keluarga Mahasiswa Teknik Elektro dan Teknologi Informasi, Fakultas Teknik, Universitas Gadjah Mada yang selanjutnya disebut KPU adalah suatu komisi yang menyelenggarakan pemilu berdasarkan Undang-Undang Pemilu. Pada tahun ini KPU KMTETI mengusung tema Technocracy. Technocracy sendiri memiliki arti “Navigating TETI’s Road to A New Leader” merupakan akronim dari technology dan democracy. TECHNOCRACY menggabungkan inovasi teknologi dan semangat demokrasi yang diwujudkan dalam Pemilihan Ketua KMTETI yang dijalankan secara adil, transparan, dan penuh integritas. Melalui TECHNOCRACY, diharapkan dapat terpilih Ketua KMTETI 2025 yang mampu berdiri di garis depan perubahan dan membawa KMTETI menuju masa depan yang lebih baik dengan semangat keadilan.` 
-        },
-        section3 : {
-            title1 : `TUGAS DAN WEWENANG`,
-            title2 : `KPU KMTETI`,
-            description : (
-                <ol>
-                    <li>1. Merencanakan, mengkoordinasikan, dan memantau penyelenggaraan pemilu.</li>
-                    <li>2. Menetapkan, mengkoordinasikan, menyelenggarakan, dan mengendalikan semua tahapan pelaksanaan pemilu.</li>
-                    <li>3. Menetapkan daftar pemilih.</li>
-                    <li>4. Menetapkan peserta pemilu.</li>
-                    <li>5. Merancang SPS.</li>
-                    <li>6. Menetapkan waktu, tanggal, tata cara pelaksanaan kampanye, dan pemungutan suara.</li>
-                    <li>7. Mengumumkan Ketua PH KMTETI FT UGM terpilih.</li>
-                    <li>8. Melakukan evaluasi dan pelaporan pelaksanaan pemilu.</li>
-                    <li>9. Melaksanakan tugas dan kewenangan yang diatur Undang-Undang Pemilu.</li>
-                    <li>10. Menyusun peraturan yang belum diatur dalam Undang-Undang Pemilu atas persetujuan Ketua PH KMTETI FT UGM</li>
-                </ol>
-            )
-        },
-        section4 : {
-            title1 : `ORGANIZING COMMITTEE`,
-            title2 : `KPU KMTETI 2024`,
-            description : ''
-        },
+    section2: {
+      title1: `KOMISI PEMILIHAN UMUM`,
+      title2: `KMTETI 2024`,
+      description: `Komisi Pemilihan Umum Keluarga Mahasiswa Teknik Elektro dan Teknologi Informasi, Fakultas Teknik, Universitas Gadjah Mada yang selanjutnya disebut KPU adalah suatu komisi yang menyelenggarakan pemilu berdasarkan Undang-Undang Pemilu. Pada tahun ini KPU KMTETI mengusung tema Technocracy. Technocracy sendiri memiliki arti “Navigating TETI’s Road to A New Leader” merupakan akronim dari technology dan democracy. TECHNOCRACY menggabungkan inovasi teknologi dan semangat demokrasi yang diwujudkan dalam Pemilihan Ketua KMTETI yang dijalankan secara adil, transparan, dan penuh integritas. Melalui TECHNOCRACY, diharapkan dapat terpilih Ketua KMTETI 2025 yang mampu berdiri di garis depan perubahan dan membawa KMTETI menuju masa depan yang lebih baik dengan semangat keadilan.`
     },
-
-    tataCara : {
-        section1 : {   
-            title1 : `TATA CARA PEMILU`,
-            title2 : `KETUA KMTETI 2024`,
-            description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
-        },
+    section3: {
+      title1: `TUGAS DAN WEWENANG`,
+      title2: `KPU KMTETI`,
+      description: (
+        <ol>
+          <li>1. Merencanakan, mengkoordinasikan, dan memantau penyelenggaraan pemilu.</li>
+          <li>2. Menetapkan, mengkoordinasikan, menyelenggarakan, dan mengendalikan semua tahapan pelaksanaan pemilu.
+          </li>
+          <li>3. Menetapkan daftar pemilih.</li>
+          <li>4. Menetapkan peserta pemilu.</li>
+          <li>5. Merancang SPS.</li>
+          <li>6. Menetapkan waktu, tanggal, tata cara pelaksanaan kampanye, dan pemungutan suara.</li>
+          <li>7. Mengumumkan Ketua PH KMTETI FT UGM terpilih.</li>
+          <li>8. Melakukan evaluasi dan pelaporan pelaksanaan pemilu.</li>
+          <li>9. Melaksanakan tugas dan kewenangan yang diatur Undang-Undang Pemilu.</li>
+          <li>10. Menyusun peraturan yang belum diatur dalam Undang-Undang Pemilu atas persetujuan Ketua PH KMTETI FT
+            UGM
+          </li>
+        </ol>
+      )
     },
+    section4: {
+      title1: `ORGANIZING COMMITTEE`,
+      title2: `KPU KMTETI 2024`,
+      description: ''
+    },
+  },
+  candidate: {
+    c1: {
+      vm: `Visi: Mewujudkan KMTETI menjadi lingkungan dinamis dan inklusif yang menjadi wadah perkembangan skill profesional serta aplikatif.\n\nMisi:\n1. Membentuk lingkungan yang mendorong perpaduan antara keahlian profesional dan pengembangan keterampilan.\n2. Meningkatkan rasa kebersamaan, kepedulian, dan saling memiliki antar mahasiswa warga DTETI.\n3. Menjalin kerjasama dan membangun relasi dengan berbagai pihak, baik internal maupun eksternal.\n4. Membangun koordinasi yang aktif dan berkesinambungan secara transparan dengan seluruh masyarakat KMTETI.\n5. Mengoptimalkan minat, bakat, serta potensi yang dimiliki anggota KMTETI.`,
+      cv: "https://drive.google.com/file/d/1EyQaUnKITEoMTibGqApkdjoQoxIn0ToG/view?usp=sharing",
+      orasi: "https://www.youtube.com/watch?v=dQw4w9WgXcQ", // Placeholder video
+    },
+    c2: {
+      vm: `Visi: Menjadikan KMTETI sebagai wadah yang inklusif, berdaya saing, dan berorientasi pada pengembangan diri serta profesionalisme, yang mampu merangkul dan memberikan manfaat nyata bagi seluruh mahasiswa TETI.\n\nMisi:\n1. Membangun rasa kekeluargaan dan inklusivitas melalui berbagai kegiatan yang dapat mempererat hubungan antar anggota, sehingga tercipta lingkungan yang harmonis.\n2. Menjadikan KMTETI sebagai rumah bagi seluruh mahasiswa TETI untuk berkreasi, berinovasi dan mengembangkan potensi diri.\n3. Membangun nilai profesionalisme untuk menciptakan KMTETI yang berkelanjutan.\n4. Menjalin kerjasama dengan berbagai pihak, baik internal maupun eksternal, untuk memperluas jaringan dan peluang pengembangan anggota.\n5. Membangun sinergi dengan berbagai pihak untuk meningkatkan visibilitas.`,
+      cv: "https://drive.google.com/file/d/1Qe-HU5OG-QxxIaL6vl1CERAngxocluT6/view?usp=sharing",
+      orasi: "https://www.youtube.com/watch?v=dQw4w9WgXcQ", // Placeholder video
+    },
+  },
+  tataCara: {
+    section1: {
+      title1: `TATA CARA PEMILU`,
+      title2: `KETUA KMTETI 2024`,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+    },
+  },
 }

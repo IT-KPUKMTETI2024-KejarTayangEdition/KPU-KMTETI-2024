7305381dc40d92d7741acee3f26b62956c191206
aevryiam <ilhamyusufwiam@mail.ugm.ac.id>
2024-11-18 22:19:32 +0700

    fix : adjust for mobile

--- app/laporkan/page.js
+++ app/laporkan/page.js
@@ -20,7 +20,7 @@ export default function ReportingPage() {
     <main className="flex flex-col w-screen text-5xl rounded-none max-md:text-4xl">
       <section className="flex relative flex-col justify-center items-center px-20 py-16 w-full">
         <div className="flex text-center md:justify-center relative ml-3.5 w-full mb-2.5">
-          <h1 className={`text-primary-light md:text-8xl font-bold text-4xl ${dalekpinpoint.className}`}>
+          <h1 className={`text-primary-light md:text-8xl font-bold text-6xl ${dalekpinpoint.className}`}>
             Ajukan Laporan
           </h1>
         </div>
@@ -42,7 +42,7 @@ export default function ReportingPage() {
 
 function ReportButton({ text, className, href }) {
   return (
-    <Link href={href} className={`md:px-16 md:py-5 md:mt-16 max-w-full text-center bg-primary-dark rounded-3xl border-4 border-woodbrown text-woodbrown w-[728px] px-5 mt-10 text-4xl ${className}`}>
+    <Link href={href} className={`md:px-16 md:py-5 md:mt-16 max-w-full text-center bg-primary-dark rounded-3xl border-4 border-woodbrown text-woodbrown w-[728px] px-5 py-5 mt-10 text-4xl ${className}`}>
       {text}
     </Link>
   );

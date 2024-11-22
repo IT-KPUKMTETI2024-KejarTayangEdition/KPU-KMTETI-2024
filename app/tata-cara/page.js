7304cef73d5ba384f6a4382f74dba11ae2c03ca0
aevryiam <ilhamyusufwiam@mail.ugm.ac.id>
2024-11-22 20:12:15 +0700

    fix: anggep kelar

--- app/tata-cara/page.js
+++ app/tata-cara/page.js
@@ -1,17 +1,37 @@
+"use client"
+
+import { useEffect, useState } from "react";
+
 const Page = () => {
+  const [isPdfSupported, setIsPdfSupported] = useState(true);
+
+  useEffect(() => {
+    const obj = document.createElement("object");
+    obj.type = "application/pdf";
+    setIsPdfSupported(obj.type === "application/pdf");
+  }, []);
+
   return (
     <div className="md:panduan-wrapper flex justify-center items-center">
-      <object
-        data="/tatacara.pdf"
-        type="application/pdf"
-        className="md:w-full md:h-[90vh] lg:max-w-[100%] min-h-screen h-[50vh] w-[50vh]"
-      >
+      {isPdfSupported ? (
+        <object
+          data="/tatacara.pdf"
+          type="application/pdf"
+          className="md:w-full md:h-[90vh] lg:max-w-[100%] min-h-screen h-[50vh] w-[50vh]"
+        >
+          <p className="download-pdf text-center min-h-screen min-w-screen">
+            Unable to display PDF file. <a href="/tatacara.pdf" className="text-blue-500 underline">Download</a>{" "}
+            instead.
+          </p>
+        </object>
+      ) : (
         <p className="download-pdf text-center min-h-screen min-w-screen">
           Unable to display PDF file. <a href="/tatacara.pdf" className="text-blue-500 underline">Download</a>{" "}
           instead.
         </p>
-      </object>
+      )}
     </div>
   );
 };
+
 export default Page;

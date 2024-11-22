26e0c3a34948ad518dc631e88ab6547b333dc07e
aevryiam <ilhamyusufwiam@mail.ugm.ac.id>
2024-11-22 20:01:58 +0700

    fix: balikin ke awal, f ip

--- app/tata-cara/page.js
+++ app/tata-cara/page.js
@@ -1,16 +1,16 @@
 const Page = () => {
   return (
     <div className="md:panduan-wrapper flex justify-center items-center">
-      <iframe
-        src="/tatacara.pdf"
+      <object
+        data="/tatacara.pdf"
+        type="application/pdf"
         className="md:w-full md:h-[90vh] lg:max-w-[100%] min-h-screen h-[50vh] w-[50vh]"
-        title="PDF Viewer"
       >
         <p className="download-pdf text-center min-h-screen min-w-screen">
           Unable to display PDF file. <a href="/tatacara.pdf" className="text-blue-500 underline">Download</a>{" "}
           instead.
         </p>
-      </iframe>
+      </object>
     </div>
   );
 };

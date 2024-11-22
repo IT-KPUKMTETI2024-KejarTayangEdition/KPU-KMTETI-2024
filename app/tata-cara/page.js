83aa7eb32e6905f518824b2438fcfd2ed30bdc5d
aevryiam <ilhamyusufwiam@mail.ugm.ac.id>
2024-11-22 11:40:55 +0700

    fix: display on mobile

--- app/tata-cara/page.js
+++ app/tata-cara/page.js
@@ -6,7 +6,7 @@ const Page = () => {
         type="application/pdf"
         className="md:w-full sm:h-[85vh] md:max-w-[100%] max-w-100%"
       >
-        <p className="download-pdf mb-28 mt-28">
+        <p className="download-pdf min-h-screen">
           Unable to display PDF file. <a href="/tatacara.pdf" className="text-blue-500 underline">Download</a>{" "}
           instead.
         </p>

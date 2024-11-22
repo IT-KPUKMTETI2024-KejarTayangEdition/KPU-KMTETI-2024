48a752b03d9c8c9052aa781b1165f034e8f2d9f0
aevryiam <ilhamyusufwiam@mail.ugm.ac.id>
2024-11-22 18:50:26 +0700

    fix: nyoba

--- app/tata-cara/page.js
+++ app/tata-cara/page.js
@@ -2,11 +2,11 @@ import "./content.css";
 
 const Page = () => {
   return (
-    <div className="md:panduan-wrapper">
+    <div className="md:panduan-wrapper flex justify-center items-center">
       <object
         data="/tatacara.pdf"
         type="application/pdf"
-        className="md:w-full h-[85vh] max-w-[100%]"
+        className="md:w-full h-[85vh] max-w-[95%]"
       >
         <p className="download-pdf text-center min-h-screen min-w-screen">
           Unable to display PDF file. <a href="/tatacara.pdf" className="text-blue-500 underline">Download</a>{" "}

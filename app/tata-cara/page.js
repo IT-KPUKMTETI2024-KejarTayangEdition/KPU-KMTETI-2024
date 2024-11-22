02e4b972f7931d405216b2e78ffe114e237963bc
aevryiam <ilhamyusufwiam@mail.ugm.ac.id>
2024-11-22 19:52:44 +0700

    fix: nyoba benerin lagi :(

--- app/tata-cara/page.js
+++ app/tata-cara/page.js
@@ -1,12 +1,10 @@
-import "./content.css";
-
 const Page = () => {
   return (
     <div className="md:panduan-wrapper flex justify-center items-center">
       <object
         data="/tatacara.pdf"
         type="application/pdf"
-        className="md:w-full h-[85vh] max-w-[95%]"
+        className="md:w-full md:h-[90vh] lg:max-w-[100%] min-h-screen h-[50vh] w-[50vh]"
       >
         <p className="download-pdf text-center min-h-screen min-w-screen">
           Unable to display PDF file. <a href="/tatacara.pdf" className="text-blue-500 underline">Download</a>{" "}

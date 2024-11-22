bd400f06df89fb25af98270e15edbe7c679156a3
aevryiam <ilhamyusufwiam@mail.ugm.ac.id>
2024-11-22 19:56:14 +0700

    fix: nyoba lagi

--- app/tata-cara/page.js
+++ app/tata-cara/page.js
@@ -1,16 +1,16 @@
 const Page = () => {
   return (
     <div className="md:panduan-wrapper flex justify-center items-center">
-      <object
-        data="/tatacara.pdf"
-        type="application/pdf"
+      <iframe
+        src="/tatacara.pdf"
         className="md:w-full md:h-[90vh] lg:max-w-[100%] min-h-screen h-[50vh] w-[50vh]"
+        title="PDF Viewer"
       >
         <p className="download-pdf text-center min-h-screen min-w-screen">
           Unable to display PDF file. <a href="/tatacara.pdf" className="text-blue-500 underline">Download</a>{" "}
           instead.
         </p>
-      </object>
+      </iframe>
     </div>
   );
 };

20ce0c16b165172429d0e63a4c29efaa484b5d13
aevryiam <ilhamyusufwiam@mail.ugm.ac.id>
2024-11-22 14:11:09 +0700

    fix: change back (ip still not fixed)

--- app/tata-cara/page.js
+++ app/tata-cara/page.js
@@ -6,7 +6,7 @@ const Page = () => {
         type="application/pdf"
         className="md:w-full sm:h-[85vh] md:max-w-[100%] max-w-100%"
       >
-        <p className="download-pdf my-32">
+        <p className="download-pdf text-center min-h-screen min-w-screen">
           Unable to display PDF file. <a href="/tatacara.pdf" className="text-blue-500 underline">Download</a>{" "}
           instead.
         </p>
